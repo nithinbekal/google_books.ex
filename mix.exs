a0f997f4326395c40d49863e55d70b4da7e5e764
Nithin Bekal <nithinbekal@gmail.com>
2016-06-09 21:42:43 +0530

    Remove build_embedded and start_permanent options from mix.exs

--- mix.exs
+++ mix.exs
@@ -5,8 +5,6 @@ defmodule GoogleBooks.Mixfile do
     [app: :google_books,
      version: "0.0.1",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
