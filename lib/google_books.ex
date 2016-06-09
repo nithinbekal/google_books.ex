40d5332b1bfab539e53dba3033b51d3fcb38ee23
Nithin Bekal <nithinbekal@gmail.com>
2016-06-09 19:06:06 +0530

    Add GoogleBooks.find_by_isbn/1

--- lib/google_books.ex
+++ lib/google_books.ex
@@ -1,2 +1,23 @@
 defmodule GoogleBooks do
+  @api_url "https://www.googleapis.com/books/v1/volumes"
+
+  def find_by_isbn(isbn) do
+    isbn
+    |> remove_dashes
+    |> build_url
+    |> get_json
+    |> decode_response
+    |> extract_volume_info
+  end
+
+  defp remove_dashes(isbn), do: String.replace(isbn, "-", "")
+  defp build_url(isbn), do: "#{@api_url}?q=isbn:#{isbn}"
+  defp get_json(url), do: HTTPoison.get!(url).body
+  defp decode_response(json), do: Poison.decode!(json)
+
+  defp extract_volume_info(%{"totalItems" => 0}), do: :not_found
+
+  defp extract_volume_info(response) do
+    hd(response["items"])["volumeInfo"]
+  end
 end
