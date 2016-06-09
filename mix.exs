5c84f9f9040734211ad2755fda67db7cdbf94dc3
Nithin Bekal <nithinbekal@gmail.com>
2016-06-09 21:46:44 +0530

    Add package information

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule GoogleBooks.Mixfile do
     [app: :google_books,
      version: "0.0.1",
      elixir: "~> 1.2",
+     description: "A simple wrapper for Google Books API",
+     package: package,
      deps: deps]
   end
 
@@ -16,6 +18,15 @@ defmodule GoogleBooks.Mixfile do
     [
       {:httpoison, "~> 0.8.0"},
       {:poison, "~> 2.0"},
+    ]
+  end
+
+  def package do
+    [ name: :google_books,
+      files: ["lib", "mix.exs"],
+      maintainers: ["Nithin Bekal"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/nithinbekal/google_books.ex"},
     ]
   end
 end
